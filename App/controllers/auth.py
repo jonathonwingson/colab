044ec8f52df5d12376990e97ebabfd40a428de5b
jWing-ML <jonathon.wingson@my.uwi.edu>
2022-04-23 06:18:23 +0000

    yea

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -3,7 +3,6 @@ from flask_jwt import JWT
 from App.models import User
 
 
-
 def authenticate(email, password):
     user = User.query.filter_by(email=email).first()
     if user and user.check_password(password):
